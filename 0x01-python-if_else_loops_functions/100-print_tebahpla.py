b66767c0fac49a7fff23da73df4959166c41431c
chandem <49794721+chandem@users.noreply.github.com>
2022-09-20 19:31:09 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Author - Chane Eshetu
-def add(a, b):
-    """Return the addition of a and b."""
-    return (a + b)
+# Author -Bamidele Adefolaju
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0

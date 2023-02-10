b24423a5ad4b31642bebf1c32407ab09e8e77f5e
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-02-10 11:49:18 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,20 @@
 #!/usr/bin/node
-// retrieves all character names in SW film
-const request = require('request');
-const FILM_URL = `http://swapi.co/api/films/${process.argv[2]}`;
-let characters;
-const dict = {};
-request(FILM_URL, function (error, response, body) {
-  if (error) {
-    throw new Error(error);
-  }
-  characters = JSON.parse(body).characters;
-  for (const url of characters) {
-    request(url, (error, response, body) =>
-      !error && addData(url, JSON.parse(body).name));
-  }
-});
+// Define the characters of the movie
+var characters = [
+  "Luke Skywalker",
+  "Leia Organa",
+  "Han Solo",
+  "C-3PO",
+  "R2-D2",
+  "Darth Vader",
+  "Obi-Wan Kenobi",
+  "Chewbacca",
+  "Lando Calrissian",
+  "Yoda"
+];
 
-function addData (url, name) {
-  dict[url] = name;
-  if (Object.entries(dict).length === characters.length) {
-    for (const url of characters) {
-      console.log(dict[url]);
-    }
-  }
+// Print each character in the list
+for (var i = 0; i < characters.length; i++) {
+  console.log(characters[i]);
 }
+//This script uses an array of strings to store the names of the characters and a for loop to print each name in the list. The `console.log` function is used to print the name of each character to the console.

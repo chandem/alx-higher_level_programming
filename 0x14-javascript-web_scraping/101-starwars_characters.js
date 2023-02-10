f16a36f16eba61e76d6665674d9b9f3a3a53272f
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-02-10 11:56:52 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
-// Define the characters of the movie
-var characters = [
-  "Luke Skywalker",
-  "Leia Organa",
-  "Han Solo",
-  "C-3PO",
-  "R2-D2",
-  "Darth Vader",
-  "Obi-Wan Kenobi",
-  "Chewbacca",
-  "Lando Calrissian",
-  "Yoda"
-];
+//Here is the script in JavaScript that will print out all the characters of a Star Wars movie:
 
-// Print each character in the list
-for (var i = 0; i < characters.length; i++) {
-  console.log(characters[i]);
-}
-//This script uses an array of strings to store the names of the characters and a for loop to print each name in the list. The `console.log` function is used to print the name of each character to the console.
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+    characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
+
+
+ 
